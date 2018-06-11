0a6397025c8a0d39e2f19a7eeebb4f2829a03e0a
abdesselam <xpack94@gmail.com>
2018-06-11 01:25:13 -0400

    adding check_offset function

--- index.js
+++ index.js
@@ -6,14 +6,23 @@ window.onload = function(e) {
 
 //function that makes the navbar sticks to the top when scrolling
 function stickyNav(e){
+   
     let navbar=document.getElementById("navbar");
     let navbarOffset=navbar.offsetTop;
+    
+     check_offset(pageYOffset,navbarOffset);
+    
       document.addEventListener("scroll",function(e){
-         if(pageYOffset>=navbarOffset){
+        check_offset(pageYOffset,navbarOffset);
+      });
+
+}
+//compare between two offsets 
+function check_offset(pageOffset,navbarOffset){
+     let navbar=document.getElementById("navbar");
+     if(pageOffset>=navbarOffset){
             navbar.classList.add("fix");
          }else{
              navbar.classList.remove("fix");
          }
-      });
-
 }
