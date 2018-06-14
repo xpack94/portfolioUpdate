fb79f80c4e560af9d69bd91df7bcdfba2744694a
abdesselam <xpack94@gmail.com>
2018-06-14 05:19:19 -0400

    adding smooth scrolling when clicking on take a tour button

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ window.onload = function(e) {
 
     stickyNav();
     smoothScrolling();
+    scroll_on_tour_press();
 
 };
 
@@ -41,6 +42,19 @@ function scrollTo(e){
     e.preventDefault();
     let section=document.getElementById(e.target.getAttribute("href").substring(1));
     section.scrollIntoView({ behavior: 'smooth', block: 'start', inline: "nearest"  });
+    
+    
+}
+
+function scroll_on_tour_press(){
+    let button = document.querySelector(".tour");
+    let landing_page = document.querySelector(".landing_page");
+    button.addEventListener("click",(e)=>{
+        e.preventDefault();
+        window.scroll({
+                       top:landing_page.clientHeight,
+                       behavior:'smooth'});    
+    });
     
     
 }
